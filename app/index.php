51b38b2b0d26433997e856eb798de365153929e5
effree <evolved.wd@gmail.com>
2023-09-07 18:11:32 -0400

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,37 @@
+<html><head>
+    <meta charset="utf-8">
+    <title>Tautulli - Home Stats</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="http://localhost:8181/css/bootstrap3/bootstrap.min.css" rel="stylesheet">
+    <link href="http://localhost:8181/css/pnotify.custom.min.css" rel="stylesheet">
+    <link href="http://localhost:8181/css/selectize.bootstrap3.css" rel="stylesheet">
+    <link href="http://localhost:8181/css/selectize.min.css" rel="stylesheet">
+    <link href="http://localhost:8181/css/tautulli.css?v2.13.1" rel="stylesheet">
+    <link href="http://localhost:8181/css/opensans.min.css" rel="stylesheet">
+    <link href="http://localhost:8181/css/font-awesome.all.min.css" rel="stylesheet">
+    <link href="http://localhost:8181/css/font-awesome.v4-shims.min.css" rel="stylesheet">
+    
+<link href="http://localhost:8181/css/jquery.scrollbar.css" rel="stylesheet">
+
+
+    <!-- Favicons -->
+    <link rel="icon" type="image/png" sizes="32x32" href="http://localhost:8181/images/favicon/favicon-32x32.png?v=2.6.0">
+    <link rel="icon" type="image/png" sizes="16x16" href="http://localhost:8181/images/favicon/favicon-16x16.png?v=2.6.0">
+    <link rel="shortcut icon" href="http://localhost:8181/images/favicon/favicon.ico?v=2.6.0">
+
+    <!-- ICONS -->
+    <!-- Android -->
+    <link rel="manifest" href="http://localhost:8181/images/favicon/manifest.json?v=2.9.0" crossorigin="use-credentials">
+    <meta name="theme-color" content="#282a2d">
+    <!-- Apple -->
+    <link rel="apple-touch-icon" sizes="180x180" href="http://localhost:8181/images/favicon/apple-touch-icon.png?v=2.6.0">
+    <link rel="mask-icon" href="http://localhost:8181/images/favicon/safari-pinned-tab.svg?v=2.6.0" color="#282a2d">
+    <meta name="apple-mobile-web-app-title" content="Tautulli">
+    <!-- Microsoft -->
+    <meta name="application-name" content="Tautulli">
+    <meta name="msapplication-config" content="http://localhost:8181/images/favicon/browserconfig.xml?v=2.6.0">
+</head><body>
+  <?php include('http://localhost:8181/home_stats'); ?>
+</body></html>
